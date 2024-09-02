cb099de3e84eb2d19c3a2eda8914a409e168c755
Elizabeth <lesep.28@gmail.com>
2024-09-02 15:44:17 -0500

    avance integrador

---
+++ src/main/java/dh/backend/clinica/service/ITurnoService.java
@@ -0,0 +1,16 @@
+package dh.backend.clinica.service;
+
+import dh.backend.clinica.entity.Turno;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ITurnoService {
+    Turno guardarTurno(Turno turno);
+    Optional<Turno> buscarPorId(Integer id);
+    List<Turno> buscarTodos();
+
+    void modificarTurno(Turno turno);
+
+    void eliminarTurno(Integer id);
+}
---
+++ src/main/java/dh/backend/clinica/service/impl/OdontologoService.java
@@ -0,0 +1,44 @@
+package dh.backend.clinica.service.impl;
+
+
+import dh.backend.clinica.entity.Odontologo;
+import dh.backend.clinica.repository.IOdontologoRepository;
+import dh.backend.clinica.service.IOdontologoService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OdontologoService implements IOdontologoService {
+    private IOdontologoRepository odontologoRepository;
+
+    public OdontologoService(IOdontologoRepository odontologoRepository) {
+        this.odontologoRepository = odontologoRepository;
+    }
+
+    @Override
+    public Odontologo guardarOdontologo(Odontologo odontologo) {
+        return odontologoRepository.save(odontologo);
+    }
+
+    @Override
+    public Optional<Odontologo> buscarPorId(Integer id) {
+        return odontologoRepository.findById(id);
+    }
+
+    @Override
+    public List<Odontologo> buscarTodos() {
+        return odontologoRepository.findAll();
+    }
+
+    @Override
+    public void modificarOdontologo(Odontologo odontologo) {
+        odontologoRepository.save(odontologo);
+    }
+
+    @Override
+    public void eliminarOdontologo(Integer id) {
+        odontologoRepository.deleteById(id);
+    }
+}
